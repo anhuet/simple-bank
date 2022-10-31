301346de2233eb81cdc01d3e35fa80cb9886d52e
anhvl <volocanh102@gmail.com>
2022-10-31 14:23:41 +0700

    add api for account

---
+++ api/server.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	db "github.com/anhuet/simplebank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/account", server.createAccount)
+	router.GET("/account", server.ListAccounts)
+	router.GET("/account/:id", server.GetAccount)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(adress string) error {
+	return server.router.Run(adress)
+}
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"github.com/anhuet/simplebank/api"
+	db "github.com/anhuet/simplebank/db/sqlc"
+	_ "github.com/lib/pq" // <------------ here
+	"log"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://andy:secret@localhost:5433/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
 
 func main() {
-	fmt.Println("test")
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Can not connect to db", err)
+	}
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
